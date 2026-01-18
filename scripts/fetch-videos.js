41f4f5f3045206e094a08acfb143b90c934d4c2c
Johan Veldhuis <johanveldhusi@outlook.com>
2026-01-18 22:07:32 +0100

    Fix fetch-videos.js to use ES module syntax instead of CommonJS

--- scripts/fetch-videos.js
+++ scripts/fetch-videos.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 const YOUTUBE_CHANNEL_ID = process.env.YOUTUBE_CHANNEL_ID;
